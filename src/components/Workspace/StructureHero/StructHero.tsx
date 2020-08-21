fa24fee12d1787c144a2cae2ab53652972aa3437
rtviii <rtkushner@gmail.com>
2020-08-20 19:21:07 -0700

    befor extending routing

--- src/components/Workspace/StructureHero/StructHero.tsx
+++ src/components/Workspace/StructureHero/StructHero.tsx
@@ -3,6 +3,7 @@ import infoicon from "./info.svg";
 import { useSelector } from "react-redux";
 import { AppState } from "../../../redux/store";
 import "./StructHero.css";
+import { Link } from "react-router-dom";
 
 const StructHero = ({
   pdbid,
@@ -34,27 +35,32 @@ const StructHero = ({
   };
 
   return (
-    <div className={`struct-hero ${pdbid} ${str.kingdom}`} id={`_struct_${pdbid}`}>
-      <a className="tooltip">
-        <img src={infoicon} className="tt_icon" alt="tooltip" />
-        <div className="tt_content">
-          <p>Nomenclature Coverage : {nomenclatureCoverage}</p>
-          <p>Protein Chains : {proteincount}</p>
-          <p>RNA Chains : {rnacount}</p>
-          <p>Deposition: {str.pub}</p>
+    <Link to={`${pdbid}`}>
+      <div
+        className={`struct-hero ${pdbid} ${str.kingdom}`}
+        id={`_struct_${pdbid}`}
+      >
+        <a className="tooltip">
+          <img src={infoicon} className="tt_icon" alt="tooltip" />
+          <div className="tt_content">
+            <p>Nomenclature Coverage : {nomenclatureCoverage}</p>
+            <p>Protein Chains : {proteincount}</p>
+            <p>RNA Chains : {rnacount}</p>
+            <p>Deposition: {str.pub}</p>
+          </div>
+        </a>
+        <div className="pdbid_title">{pdbid}</div>
+        <div className="hero_annotations">
+          <p className="p_annot resolution">Resolution: {str.res}</p>
+          <p className="p_annot species">
+            {str.spec} |{" "}
+            <span style={{ color: kingdoms[str.kingdom][1] }}>
+              {kingdoms[str.kingdom][0]}
+            </span>
+          </p>
         </div>
-      </a>
-      <div className="pdbid_title">{pdbid}</div>
-      <div className="hero_annotations">
-        <p className="p_annot resolution">Resolution: {str.res}</p>
-        <p className="p_annot species">
-          {str.spec} |{" "}
-          <span style={{ color: kingdoms[str.kingdom][1] }}>
-            {kingdoms[str.kingdom][0]}
-          </span>
-        </p>
       </div>
-    </div>
+    </Link>
   );
 };
 
