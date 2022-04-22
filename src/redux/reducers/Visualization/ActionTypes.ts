d3ff1d5f39f221d1bfbbf412aab807d9c256dde5
Artem <rtkushner@gmail.com>
2022-04-21 20:19:54 -0700

    vis reducer

--- src/redux/reducers/Visualization/ActionTypes.ts
+++ src/redux/reducers/Visualization/ActionTypes.ts
@@ -1,61 +1,52 @@
-import { TableHeadClassKey } from "@material-ui/core";
+import { useSelector } from "react-redux";
 import { Dispatch } from "redux";
 import { getNeo4jData } from "../../AsyncActions/getNeo4jData";
-import { NeoStruct } from "../../DataInterfaces";
-import { RibosomeStructure } from "../../old_RibosomeTypes";
-import { ProteinClass, Ligand, RNAClass, RNA, Protein } from "../../RibosomeTypes";
+import { NeoStruct, PolymerMinimal } from "../../DataInterfaces";
+import { RibosomeStructure } from "../../RibosomeTypes";
+import { ProteinClass, RNAClass } from "../../RibosomeTypes";
+import { AppState, store } from "../../store";
+import { coerce_full_structure_to_neostruct } from "./VisualizationReducer";
 
-export const FULL_STRUCT_PROFILE_CHANGE = "FULL_STRUCT_PROFILE_CHANGE"
-export const FULL_COMPONENT_CHANGE  = "STRUCTURE_CHANGESTRUCTURE_CHANGE"
+export const FETCH_FULL_STRUCT_GO = "FETCH_FULL_STRUCT_GO"
+export const FETCH_FULL_STRUCT_ERR = "FETCH_FULL_STRUCT_ERR"
+export const FETCH_FULL_STRUCT_SUCCESS = "FETCH_FULL_STRUCT_SUCCESS"
+export const STRUCT_AND_COMPONENT_CHANGE = "STRUCT_AND_COMPONENT_CHANGE"
 
-export const STRUCTURE_CHANGE     = "STRUCTURE_CHANGE"
-export const PROTEIN_CHANGE       = "PROTEIN_CHANGE"
-export const RNA_CHANGE           = "RNA_CHANGE"
+export const STRUCTURE_CHANGE = "STRUCTURE_CHANGE"
+export const PROTEIN_CHANGE = "PROTEIN_CHANGE"
+export const RNA_CHANGE = "RNA_CHANGE"
 export const COMPONENT_TAB_CHANGE = "COMPONENT_TAB_CHANGE"
 
 export type VisualizationTabs = 'structure_tab' | 'protein_tab' | 'rna_tab';
 
-export interface fullStructProfileChange {
-	type                : typeof FULL_STRUCT_PROFILE_CHANGE,
-	fullStructureProfile: RibosomeStructure | null
-}
-
-export interface fullComponentChange {
-	type               : typeof FULL_COMPONENT_CHANGE,
-	fullComponentChange: RNA|Protein | null
-}
+export interface fetchFullStructGo        { type: typeof FETCH_FULL_STRUCT_GO                                                        }
+export interface fetchFullStructSuccess   { type: typeof FETCH_FULL_STRUCT_SUCCESS  , struct: RibosomeStructure                      }
+export interface fetchFullStructError     { type: typeof FETCH_FULL_STRUCT_ERR      , err: Error                                     }
+export interface structAndComponentChange { type: typeof STRUCT_AND_COMPONENT_CHANGE, struct: RibosomeStructure | null, chain: string | null }
 
 
 export interface componentTabChange {
 	type: typeof COMPONENT_TAB_CHANGE,
-	tab : VisualizationTabs,
+	tab: VisualizationTabs,
 }
 export interface structureChange {
-	type             : typeof STRUCTURE_CHANGE
-	structure        : NeoStruct | null,
+	type: typeof STRUCTURE_CHANGE
+	structure: NeoStruct | null,
 	highlighted_chain: string | null,
 }
 export interface proteinChange {
-	type  : typeof PROTEIN_CHANGE
-	class : ProteinClass | null,
+	type: typeof PROTEIN_CHANGE
+	class: ProteinClass | null,
 	parent: string | null
 }
 export interface rnaChange {
-	type  : typeof RNA_CHANGE
-	class : RNAClass | null,
+	type: typeof RNA_CHANGE
+	class: RNAClass | null,
 	parent: string | null
 }
 
-
-export const full_struct_profile_change = (struct_profile: RibosomeStructure| null):fullStructProfileChange =>({
-	type:'FULL_STRUCT_PROFILE_CHANGE',
-	fullStructureProfile:struct_profile
-})
-export const full_component_change = (component:RNA|Protein| null):fullComponentChange =>({
-	type               : 'STRUCTURE_CHANGESTRUCTURE_CHANGE',
-	fullComponentChange: component
-})
-
+export const struct_and_component_change = (struct: RibosomeStructure | null, chain: string | null) :structAndComponentChange=> ({
+	 type: 'STRUCT_AND_COMPONENT_CHANGE', struct, chain })
 
 export const protein_change = (protclass: ProteinClass | null, parent: string | null): proteinChange => ({
 	type: "PROTEIN_CHANGE",
@@ -67,13 +58,13 @@ export const rna_change = (rnaclass: RNAClass | null, parent: string | null): rn
 	parent,
 	class: rnaclass
 })
-export const struct_change = (chain: string | null, struct: NeoStruct | null): structureChange => {
-	return {
-		type             : "STRUCTURE_CHANGE",
-		structure        : struct,
-		highlighted_chain: chain
-	}
-}
+// export const struct_change = (highlighted_chain: string | null, struct: NeoStruct | null): structureChange => {
+// 	return {
+// 		type: "STRUCTURE_CHANGE",
+// 		structure: struct,
+// 		highlighted_chain: highlighted_chain
+// 	}
+// }
 export const tab_change = (tab: VisualizationTabs): componentTabChange => ({
 	type: "COMPONENT_TAB_CHANGE",
 	tab
@@ -81,54 +72,50 @@ export const tab_change = (tab: VisualizationTabs): componentTabChange => ({
 
 
 
-export const selectedStructureChange = (highlighted_chain:null|string, struct:NeoStruct|null) => {
-  return async (dispatch: Dispatch<VisualizationActions>) => {
-    dispatch({type: "STRUCTURE_CHANGE",highlighted_chain, structure:struct});
+export const fullStructureChange = (
+	struct_id                   : string | null,
+	highlight_chain_auth_asym_id: string | null
+) => {
 
 
-	if (struct === null) {
-		dispatch()
+	const currentstruct = store.getState().visualization.structure_tab.fullStructProfile
+	return async (dispatch: Dispatch<VisualizationActions>) => {
+		console.log(`Executing async. got structid :${struct_id} | chain: ${highlight_chain_auth_asym_id} | current struct :${currentstruct}`)
+		if (currentstruct?.rcsb_id === struct_id ){
+			console.log("landed in equal branch");
+			
+				dispatch(struct_and_component_change( currentstruct , highlight_chain_auth_asym_id) )
+		}
+		else if(struct_id === null) {
+			dispatch( struct_and_component_change(null, null) )
+		}
+		else {
+			console.log("requesting a new full struct", struct_id);
+			
+			getNeo4jData('neo4j', { endpoint: 'get_full_struct', params: { pdbid: struct_id } })
+				.then(
 
-	}
- 
+					response => {
+
+						console.log("fetched successfully. response:", response)
+						dispatch({
+							type: "FETCH_FULL_STRUCT_SUCCESS",
+							struct: response.data[0]
+						});
+						console.log("Attempting to coerce:");
+						dispatch(struct_and_component_change(response.data[0] , highlight_chain_auth_asym_id))
+					},
+					error => {
+						dispatch({
+							type: "FETCH_FULL_STRUCT_ERR", err: error
+						});
+					}
+				)
+
+		}
+	};
+}
 
 
-				const r = await getNeo4jData('neo4j', { endpoint: 'get_struct', params: { pdbid:struct.structure?.struct.rcsb_id as string } })
-				console.log("Got data from r:", r);
-    getNeo4jData("neo4j", {
-      endpoint: "get_rna_class", params: {
-        rna_class
-      }
-    })
-      .then(
-        response => {
-          dispatch({            type   : REQUEST_RNA_CLASS_SUCCESS,
-            payload: flattenDeep(response.data) as any,
-            rna_class
-          });
-        },
-        error => {
-          dispatch({
-            type: REQUEST_RNA_CLASS_ERR,error: error,
-          });
-        }
-      )
-  };
-};
-
-
-
-
-// //* hack
-// export interface _partialStateChange   {
-// 	type           : typeof PARTIAL_STATE_CHANGE,
-// 	statelike_slice: Partial<BindingSitesReducerState>
-// 					}
-
-export type VisualizationActions = structureChange | rnaChange | proteinChange | componentTabChange
-            // _partialStateChange
-
-// export const _partial_state_change = (statelike_slice:Partial<BindingSitesReducerState> ):_partialStateChange=>( {
-// 	type: "PARTIAL_STATE_CHANGE",
-// 	statelike_slice
-// } )
+export type VisualizationActions = structureChange | rnaChange | proteinChange | componentTabChange |
+	fetchFullStructError | fetchFullStructGo | fetchFullStructSuccess | structAndComponentChange;
