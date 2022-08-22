d88401fc43892af645121ce7b247f9906344c968
Artem <rtkushner@gmail.com>
2022-08-22 12:23:30 -0700

    added youtube-embedded tutorials to howto

--- src/components/Workspace/Display/HowTo.tsx
+++ src/components/Workspace/Display/HowTo.tsx
@@ -3,6 +3,9 @@ import React from "react";
 export const HowTo = () => {
   return (
     <div>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Ri-T4hUw0NE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/nsFLE956HM4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/IkgVysgzltY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         Supplementary materials.
     </div>
   );
